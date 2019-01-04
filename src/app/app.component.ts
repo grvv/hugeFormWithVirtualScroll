769a8b39a8cb895addc00524abf698407e04adc8
Gaurav Sharma <gaurav.gauravsharmaa@gmail.com>
2019-01-04 16:49:30 +0530

    experiment done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'virtualScroll';
+  largeDataSet = [];
+  ready = false;
+  constructor(
+    private http: HttpClient
+  ){
+
+  }
+
+  ngOnInit(){
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(
+      (res: Array<any>) => {
+        let dataSet = res;
+        for(let i = 0; i < 1000; i++){
+          this.largeDataSet = [...this.largeDataSet, ...dataSet];
+        }
+        console.log(this.largeDataSet);
+        this.ready = true;
+      }
+    )
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ScrollingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
